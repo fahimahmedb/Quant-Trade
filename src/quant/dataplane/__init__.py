973d91e3f1c3eb817e4765c3bdc2268b504d4a2b
fahimahmedb <fahimbentata@gmail.com>
2026-09-18 03:08:34 +0200

    p0: integrate SEC capture state contracts

--- src/quant/dataplane/__init__.py
+++ src/quant/dataplane/__init__.py
@@ -2,5 +2,11 @@
 
 from .panel import PricePanel, Window
 from .registry import DatasetRecord, DatasetRegistry
+from .sec_form4 import SecCapturePolicy, SecCollectorConfig, SecForm4Collector
+from .sec_form4_raw import SecAttemptRecord, SecRawObjectRecord, SecSourceVersionRecord
 
-__all__ = ["DatasetRecord", "DatasetRegistry", "PricePanel", "Window"]
+__all__ = [
+    "DatasetRecord", "DatasetRegistry", "PricePanel", "Window",
+    "SecAttemptRecord", "SecCapturePolicy", "SecCollectorConfig", "SecForm4Collector",
+    "SecRawObjectRecord", "SecSourceVersionRecord",
+]
